319569dfef07ac75623559fd3651e6ef67bb28f4
llbasull <bashumoulik@gmail.com>
2024-05-12 13:49:29 +0530

    deleted unnecesary things

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./public/**/*.{html,css,js}", "./views/**/*/.ejs"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
